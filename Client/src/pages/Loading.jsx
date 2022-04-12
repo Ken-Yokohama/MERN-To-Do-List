bd73cd3d689c3fee0d8356b9542e60e18ab811b8
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-13 04:43:19 +0800

    Added Loading Animation

--- Client/src/pages/Loading.jsx
+++ Client/src/pages/Loading.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 const Loading = () => {
+    const [loading, setLoading] = useState("Loading");
+
+    useEffect(() => {
+        setInterval(() => {
+            setTimeout(() => {
+                setLoading("Loading.");
+            }, 1000);
+            setTimeout(() => {
+                setLoading("Loading..");
+            }, 2000);
+            setTimeout(() => {
+                setLoading("Loading...");
+            }, 3000);
+        }, 4000);
+    }, []);
+
     return (
         <Box>
             <Typography variant="h4" component="h4">
-                Loading...
+                {loading}
             </Typography>
         </Box>
     );
