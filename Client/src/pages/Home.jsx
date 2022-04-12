63e15111df5297a541be1c5ed529d52c78568f15
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-13 02:51:58 +0800

    Connected Dark Mode With Background Color

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import { ListContainer } from "../containers";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+    const darkMode = useSelector((state) => state.darkMode.value);
+
     return (
         <Box
             sx={{
+                backgroundColor: darkMode ? "black" : "white",
                 height: "100%",
                 display: "flex",
                 alignItems: "center",
