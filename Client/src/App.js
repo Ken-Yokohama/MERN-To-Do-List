f9151b9121f05ff1e1b25921629f8b40b01d6b6b
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-13 02:47:39 +0800

    Connected Dark Mode State with Theme

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Main } from "./containers";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import darkModeReducer from "./features/darkModeSlice";
@@ -12,24 +11,10 @@ const store = configureStore({
 });
 
 function App() {
-    const theme = createTheme({
-        palette: {
-            mode: "dark",
-            primary: {
-                main: "#0052cc",
-            },
-            secondary: {
-                main: "#edf2ff",
-            },
-        },
-    });
-
     return (
         <React.StrictMode>
             <Provider store={store}>
-                <ThemeProvider theme={theme}>
-                    <Main />
-                </ThemeProvider>
+                <Main />
             </Provider>
         </React.StrictMode>
     );
--- Client/src/containers/Main.jsx
+++ Client/src/containers/Main.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Home, Loading } from "../pages";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { useSelector } from "react-redux";
 
 const Main = () => {
     const [backendLoaded, setBackendLoaded] = useState(false);
@@ -15,7 +17,26 @@ const Main = () => {
         };
         getToDos();
     }, []);
-    return <>{backendLoaded ? <Home /> : <Loading />}</>;
+
+    const darkMode = useSelector((state) => state.darkMode.value);
+
+    const theme = createTheme({
+        palette: {
+            mode: darkMode ? "dark" : "light",
+            primary: {
+                main: "#0052cc",
+            },
+            secondary: {
+                main: "#edf2ff",
+            },
+        },
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            {backendLoaded ? <Home /> : <Loading />}
+        </ThemeProvider>
+    );
 };
 
 export default Main;
