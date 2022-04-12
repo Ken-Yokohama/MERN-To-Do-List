a8c44f8afc2f71968db0256a50347e79d14428a0
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-13 00:20:05 +0800

    Added Folder Structure and Barrel Files

---
+++ Client/src/components/UnfinishedTasks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UnfinishedTasks = () => {
+    return <div>UnfinishedTasks</div>;
+};
+
+export default UnfinishedTasks;
---
+++ Client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as CompletedTasks } from "./CompletedTasks";
+export { default as UnfinishedTasks } from "./UnfinishedTasks";
