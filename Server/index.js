d41022d123e2ec5ba516052312858b82fc80dfde
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-12 22:22:25 +0800

    Connected to MongoDb Atlas

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(process.env.MONGODBURI);
+
 app.listen(3001, () => {
     console.log("App listening on port 3001!");
 });
